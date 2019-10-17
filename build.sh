7398fb907f7220f31fba36968684997df154f5a2
alanndz <alanmahmud0@gmail.com>
2019-10-18 00:55:36 +0800

    Big Update
    Signed-off-by: alanndz <alanmahmud0@gmail.com>

---
+++ build.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+#
+# Copyright (C) 2019 @alanndz (Telegram and Github)
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+CONF="${PWD}/config"
+
+export RELEASE_STATUS=$(cat "${CONF}/conf-release")
+export USECLANG=1
+export USEGCC=3
+export CODENAME="$(cat "${CONF}/conf-codename")"
+export KERNEL_VERSION="$(cat "${CONF}/conf-version")"
+export TYPE_KERNEL="$(cat "${CONF}/conf-type")"
+export PHONE="Redmi Note 7"
+export DEVICES="lavender"
+export CONFIG_FILE="lavender_defconfig"
+export JOBS=8
+export CUSTOM_DTB=0
+BRANCH="$(cat "${CONF}/conf-branch")"
+
+unset token
+export token=${token_tele}
+
+git clone --depth=1 -b $BRANCH https://github.com/alanndz/kernel_xiaomi_lavender saus
+cd saus
+
+wget --output-document=.kernel.sh https://raw.githubusercontent.com/alanndz/scripts/master/ci/clang.sh
+
+chmod +x .kernel.sh
+bash ./.kernel.sh

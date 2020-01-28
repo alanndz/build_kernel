d8e0c51a2e9c03b3fc324a1479dcb96668a522bf
alanndz <alanmahmud0@gmail.com>
2020-01-28 18:30:11 +0800

    build.sh: cleanup
    Signed-off-by: alanndz <alanmahmud0@gmail.com>

--- build.sh
+++ build.sh
@@ -7,12 +7,9 @@ CONF="${PWD}/config"
 
 export RELEASE_STATUS=$(cat "${CONF}/conf-release")
 export USECLANG=10
-export USEGCC=3
 export KERNEL_NAME=Fusion
-#export CODENAME="$(cat "${CONF}/conf-codename")"
 export CODENAME=Summer_Dream
 export KERNEL_VERSION="$(cat "${CONF}/conf-version")"
-# export TYPE_KERNEL="$(cat "${CONF}/conf-type")"
 export TYPE_KERNEL="HMP"
 export PHONE="Redmi Note 7"
 export DEVICES="lavender"
@@ -20,7 +17,6 @@ export CONFIG_FILE="lavender_defconfig"
 export JOBS=8
 export CUSTOM_DTB=0
 export SEND_TO_HANA_CI=true
-# BRANCH="$(cat "${CONF}/conf-branch")"
 BRANCH=new-hmp
 
 unset token
@@ -31,7 +27,7 @@ git clone --depth=1 -b $BRANCH https://${git_username}:$GIT_TOKEN@github.com/${g
 
 cd saus
 
-wget --output-document=.kernel.sh https://raw.githubusercontent.com/alanndz/scripts/master/ci/clang.sh
+wget --output-document=.kernel.sh https://raw.githubusercontent.com/alanndz/scripts/master/ci/fusion.sh
 
 chmod +x .kernel.sh
 bash ./.kernel.sh

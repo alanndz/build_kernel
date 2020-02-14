e8e3856e9f1dc6a9e0b6dbaf9369910f4b6b5657
alanndz <alanmahmud0@gmail.com>
2020-02-14 10:38:10 +0800

    Add support patch
    Signed-off-by: alanndz <alanmahmud0@gmail.com>

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@
 
 CONF="${PWD}/config"
 FOLDER=$(cat "${CONF}/folder")
+PATCHES="${PWD}/patches"
 
 export RELEASE_STATUS=$(cat "${CONF}/release")
 export USECLANG=10
@@ -17,6 +18,7 @@ export DEVICES="lavender"
 export CONFIG_FILE="lavender_defconfig"
 export JOBS=8
 export SEND_TO_HANA_CI=true
+
 BRANCH=$(cat "${CONF}/$FOLDER/branch")
 
 unset token
@@ -24,13 +26,23 @@ export token=${token_tele}
 GIT_TOKEN=$(openssl enc -base64 -d <<< ${git_token})
 
 git clone --depth=1 -b $BRANCH https://${git_username}:$GIT_TOKEN@github.com/${git_username2}/${git_repo2}.git saus
-
 cd saus
+
+RESET_COMMIT=$(echo $(git log --pretty=format:'%h'))
 
 wget --output-document=.kernel.sh https://raw.githubusercontent.com/alanndz/scripts/master/ci/fusion.sh
 
+# Build first Kernel
+
+# taking patch
+
+git am "${PATCHES}/01.patch"
+
 chmod +x .kernel.sh
 bash ./.kernel.sh
+
+# reset kernel to HEAD
+git reset --hard $RESET_COMMIT
 
 # Build second kernel for camera patch
 
@@ -38,6 +50,7 @@ export CODENAME="$(cat "${CONF}/$FOLDER/codename")_CAMERA"
 make -C "${PWD}/.ToolBuild/AnyKernel3" clean &>/dev/null
 
 # Patching kernel for new patch
-curl https://github.com/MiCode/Xiaomi_Kernel_OpenSource/commit/cf2a90f96348c6a3142d53ca209983da18c72410.patch | git am
+# curl https://github.com/MiCode/Xiaomi_Kernel_OpenSource/commit/cf2a90f96348c6a3142d53ca209983da18c72410.patch | git am
+git am "${PATCHES}/02.patch"
 
 bash ./.kernel.sh

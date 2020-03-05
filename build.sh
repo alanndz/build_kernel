9132366eb2675dea1fc02aef39717712e1f257fd
YasiR Siddiqui <38703416+Yasir-siddiqui@users.noreply.github.com>
2020-03-05 09:35:33 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -25,7 +25,7 @@ export token=${token_tele}
 GIT_TOKEN=$(openssl enc -base64 -d <<< ${git_token})
 
 # git clone --depth=1 -b $BRANCH https://${git_username}:$GIT_TOKEN@github.com/${git_link_fourteen}.git saus
-git clone --depth=1 -b 4.14-4 https://${git_username}:$GIT_TOKEN@github.com/Yasir-siddiqui/msm-4.14.git saus
+git clone --depth=1 -b msm-4.14-5 https://${git_username}:$GIT_TOKEN@github.com/Yasir-siddiqui/msm-4.14.git saus
 cd saus
 
 wget --output-document=.kernel.sh https://raw.githubusercontent.com/alanndz/scripts/master/ci/perf.sh

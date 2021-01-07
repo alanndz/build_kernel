ff7b8d8cb6a00f5527cc0fac7c380277cf53aa8a
alanndz <alanmahmud0@gmail.com>
2021-01-07 08:53:36 +0000

    gelud yuk
    Signed-off-by: alanndz <alanmahmud0@gmail.com>

--- build.sh
+++ build.sh
@@ -15,7 +15,7 @@ export KERNEL_TYPE=$(cat "${CONF}/$FOLDER/type")
 export PHONE="Redmi Note 7"
 export DEVICES="lavender"
 export CONFIG_FILE="lavender_defconfig"
-export COMPILER_IS_CLANG=true
+#export COMPILER_IS_CLANG=true
 export USECLANG="proton-11"
 export USEGCC=93
 #export CHAT_ID="-1001251953845" #
@@ -33,7 +33,7 @@ git clone --depth=1 -b $BRANCH https://${git_username}:$GIT_TOKEN@github.com/HAN
 
 cd saus
 
-wget --output-document=.kernel.sh https://raw.githubusercontent.com/alanndz/scripts/master/ci/sclang.sh
+wget --output-document=.kernel.sh https://raw.githubusercontent.com/alanndz/scripts/master/ci/perf3.sh
 
 chmod +x .kernel.sh
 bash ./.kernel.sh

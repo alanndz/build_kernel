5565da1f9c377a43b4c666998ee31451fb27b71a
alanndz <alanndz@qmail.id>
2021-02-05 15:54:59 +0000

    UPDATE: perf: branch: from wip to old
    Signed-off-by: alanndz <alanndz@qmail.id>

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ PATCHES="${PWD}/patches"
 
 export RELEASE_STATUS=$(cat "${CONF}/release")
 export RELEASE_VERSION=$(cat "${CONF}/$FOLDER/version")
-export KERNEL_NAME="Fusion"
+export KERNEL_NAME="aLn"
 export CODENAME=$(cat "${CONF}/$FOLDER/codename")-4.19
 export KERNEL_TYPE=$(cat "${CONF}/$FOLDER/type")
 export PHONE="Redmi Note 7"
@@ -21,15 +21,15 @@ export USEGCC=93
 #export CHAT_ID="-1001251953845" #
 export CHAT_ID="-1001273145812" #
 #export CHAT_ID=$(openssl enc -base64 -d <<< LTEwMDEyMzAyMDQ5MjMK)
-export DEVELOPER="alanndz-nicklas373"
-export HOST="fusion_lavender-dev"
+export DEVELOPER="alanndz"
+export HOST="lavender-dev"
 export AK_BRANCH="fusion"
 export JOBS=8
 BRANCH=$(cat "${CONF}/$FOLDER/branch")
 unset token
 export token=${token_tele}
 GIT_TOKEN=$(openssl enc -base64 -d <<< ${git_token})
-git clone --depth=1 -b $BRANCH https://${git_username}:$GIT_TOKEN@github.com/HANA-CI-Build-Project/kernel_xiaomi_lavender-4.19.git saus
+git clone --depth=1 -b $BRANCH https://${git_username}:$GIT_TOKEN@github.com/alanndz/msm-4.19.git saus
 
 cd saus
 
